4019890606bd23800d9e23fe6a4223881f6c56d6
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-01-15 06:01:18 +0700

    Update

---
+++ android/app/src/main/kotlin/com/example/textform/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.textform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/Form1.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Text Form")),
+        body: Form1(),
+      ),
+    );
+  }
+}
+
+class Form1 extends StatefulWidget {
+  const Form1({super.key});
+
+  @override
+  State<Form1> createState() => _Form1State();
+}
+
+class _Form1State extends State<Form1> {
+  String _textInput = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 8,
+        children: [
+          TextField(
+            decoration: const InputDecoration(labelText: "Input Your Name"),
+            onChanged: (value) => {
+              setState(() {
+                _textInput = value;
+              }),
+            },
+          ),
+          Text("Input: $_textInput"),
+        ],
+      ),
+    );
+  }
+}
